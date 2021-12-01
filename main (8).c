cd131854496c035f122156dd5054fd4c78af9d54
신지수 <91535442+diduuuu@users.noreply.github.com>
2021-12-01 19:46:29 +0900

    Add files via upload

---
+++ main (8).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+int a, b, count=0;
+
+scanf("%d %d",&a,&b);
+
+for (int i = a; i <= b; i++){
+  for (int j = 1; j <= i; j++) if (i%j==0) count++;
+  if(count==2) printf("%d\n",i);
+  count = 0;
+}
+
+  return 0;
+}
